3cb49c748b9c04d78025b9c807047534b6faa630
jmburbano <jose.burbano@infomediaservice.com>
2022-06-06 14:24:51 -0500

    actualizacion gateway

--- usuario-server/src/main/resources/import.sql
+++ usuario-server/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO `usuarios` (username, password) VALUES ('skandia', '$2a$10$ykhXmCAam5FUEF9GN.4Z8OwwWJidvMii6VFYe77cmS2X6oF6p4W86');
 INSERT INTO `usuarios` (username, password) VALUES ('admin', '$2a$10$qGyDfZLBB.SgLv7GCP3uZe3oM38fVtr58T1iZ1LNOvO61loNUAAaK');
 
-INSERT INTO `roles` (nombre) VALUES ('ROLE_CLIENT');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
 
 INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1, 1);
